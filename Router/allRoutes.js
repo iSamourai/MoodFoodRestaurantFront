ac971d94116a356865bf86515133bd41f708771e
Bryan <bryan85.welter@gmail.com>
2025-01-23 11:36:10 +0100

    Ajout Header

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,4 +4,4 @@ import Route from "./Route.js";
 export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Quai Antique";
+export const websiteName = "Mood Food";
