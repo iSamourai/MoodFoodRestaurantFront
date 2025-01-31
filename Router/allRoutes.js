72ef22707b0d23958482ddea37e1aa4fc9c056f8
Bryan <bryan85.welter@gmail.com>
2025-01-31 13:47:09 +0100

    Ajout Page Mon compte et Modifier mot de passe

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
   new Route("/galerie", "Galerie", "/pages/galerie.html"),
   new Route("/signin", "Connexion", "/pages/signin.html"),
   new Route("/signup", "Deconnexion", "/pages/signup.html"),
+  new Route("/account", "Mon Compte", "/pages/account.html"),
+  new Route("/editpassword", "Modification", "/pages/editpassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
